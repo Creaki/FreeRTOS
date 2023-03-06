21a0833ea7202f3fcf817a3ed225d48cc006f265
Creak <1445255114@qq.com>
2023-03-06 12:33:42 +0800

    添加项目文件。

---
+++ FreeRTOS/FreeRTOS.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+#if 0
+/***********************堆***********************/
+char heap_buf[1024];
+
+int pos = 0;
+
+void* my_malloc(int size)
+{
+	int old_pos = pos;
+	pos += size;
+
+	return &heap_buf[old_pos];
+}
+
+void my_free(void* buf)
+{
+	//free
+}
+
+int main(void)
+{
+	char ch = 'A'; //ch = 65
+	char* buf = my_malloc(100);
+	int i = 0;
+
+	for (i = 0; i < 26; i++)
+	{
+		buf[i] = ch + i;
+		printf("%c\n", buf[i]);
+	}
+
+	my_free(buf);
+	return 0;
+}
+
+#elif 1
+#endif
